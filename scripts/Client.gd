feb079e0cf763f9b6061003f58ae8ea62da7f5d7
Vlad-at-PJATK <s23401@pjwstk.edu.pl>
2020-10-31 20:39:55 +0100

    Kuk

--- scripts/Client.gd
+++ scripts/Client.gd
@@ -1,21 +1,22 @@
 extends KinematicBody2D
 
+
+
+var poss = Smpts
 var names = ["Kaka", "Sraka", "Ochlo", "Yebun", "Pukich"]
 var socalling = ""
 var illness = []
 
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 func give_smpt():
+	poss = poss.genetate()
 	for i in range(3):
 		illness.append()
 		pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(names[0])
 	pass # Replace with function body.
 
 
--- scripts/Smpt.gd
+++ scripts/Smpt.gd
@@ -3,9 +3,14 @@ extends Resource
 class_name Smpt
 
 export(String) var name = ""
-export(String) var cure = ""
+export(String) var cures = []
+export(String) var anticures = []
 export(String) var visual = false
 
 
-func _ready():
-	pass
+func new(new_name, new_cure, new_anticure, is_visual):
+	name = new_name
+	cures = new_cure
+	anticures = new_anticure
+	visual = is_visual
+	
