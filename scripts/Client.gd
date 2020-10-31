800e25989f573f1fe9301b6b6d4e4209e6f7f383
Vlad-at-PJATK <s23401@pjwstk.edu.pl>
2020-10-31 18:27:42 +0100

    Poop

---
+++ scripts/Client.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+var names = ["Kaka", "Sraka", "Ochlo", "Yebun", "Pukich"]
+var socalling = ""
+var illness = []
+
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+func give_smpt():
+	for i in range(3):
+		illness.append()
+		pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ scripts/Smpt.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name Smpt
+
+export(String) var name = ""
+export(String) var cure = ""
+export(String) var visual = false
+
+
+func _ready():
+	pass
